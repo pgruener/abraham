b46f4dd5ff74f2a1d5a61e2f6ed87aac528a5db3
Philipp Gruener <philipp.gruener@royal-software.de>
2020-07-29 13:59:04 +0200

    Repos should make sure, JS dependencies are included before abraham.

--- app/assets/javascripts/abraham/index.js
+++ app/assets/javascripts/abraham/index.js
@@ -1,7 +1,3 @@
-//= require jquery
-//= require js-cookie/src/js.cookie
-//= require shepherd.js/dist/js/shepherd
-
 $(document).on('turbolinks:before-cache', function() {
     // Remove visible product tours
     $(".shepherd-step").remove();
